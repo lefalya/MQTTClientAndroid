1563efb9e90ec75cc4d352d4f8543cd2a70102ed
Bernino Falya <falya.bernino@gmail.com>
2019-09-12 20:07:56 +0700

    added button feature

--- app/src/main/java/com/example/mqttpahoexample/MainActivity.java
+++ app/src/main/java/com/example/mqttpahoexample/MainActivity.java
@@ -1,23 +1,21 @@
 package com.example.mqttpahoexample;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
-
-import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,20 +25,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        MemoryPersistence memPer = new MemoryPersistence();
-        String clientId = MqttClient.generateClientId();
+        final MemoryPersistence memPer = new MemoryPersistence();
+        final String clientId = MqttClient.generateClientId();
         final MqttAndroidClient client = new MqttAndroidClient(
-                this.getApplicationContext(), "tcp://192.168.121.133:1883", clientId, memPer);
+                this.getApplicationContext(),
+                "tcp://192.168.121.133:1883",
+                clientId,
+                memPer);
 
+        Button ledSwitch = (Button) findViewById(R.id.ledSwitch);
 
+        ledSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendCommand(client, clientId, memPer);
+            }
+        });
+    }
 
+    private void sendCommand (final MqttAndroidClient client, String clientId, MemoryPersistence memPer){
+        Log.i("MSSG", "SMG");
         try {
             client.connect(null, new IMqttActionListener() {
 
                 @Override
                 public void onSuccess(IMqttToken mqttToken) {
 
-                    MqttMessage message = new MqttMessage("Hello, I am Android Mqtt Client.".getBytes());
+                    MqttMessage message = new MqttMessage("LED ON".getBytes());
                     message.setQos(2);
                     message.setRetained(false);
 
